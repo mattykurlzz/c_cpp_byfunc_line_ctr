4c47da09b1eaed068f5b260b1ef4a27f96c6aaa9
mattykurlzz <63931003+mattykurlzz@users.noreply.github.com>
2024-12-10 12:03:57 +0300

    Update count_lines.py

--- count_lines.py
+++ count_lines.py
@@ -8,42 +8,68 @@ os.chdir(folder_path)
 
 
 def extract_functions(file_path):
-    start_line, end_line, line_ctr, par_ctr = 0, 0, -1, 0
-    func_started = False
-    func_name = ""
-    with open(file_path, "rb") as f:
-        contents = f.readlines()
-        for line in contents:
-            line_ctr += 1
-            if re.search("^(((([a-zA-Z0-9_&:(\*)]+ )*)?[a-zA-Z0-9_+-=<>]+)|([a-zA-Z0-9_&: ]*[a-zA-Z0-9_+-=<>]+::[a-zA-Z0-9_]+)) *\([A-Za-z0-9_ ,&(\*):]*(\) *{?)?( */.*)?\r?\n$", line.decode()):
-                func_started = True
-                print(line)
-                func_name = re.search("([A-Za-z0-9_]+ )*[A-Za-z0-9_]+(\(|:)", line.decode()).string.split('(')[0].split(' ')[-1] #re.findall("[a-z_]+ ?\(.*\) {\r\n$", line.decode())[0].split('(')[0]
-                print(func_name + " found on " + str(line_ctr))
-                start_line = line_ctr
-            if func_started and re.search("{", line.decode()):
-                par_ctr += re.findall("{", line.decode()).__len__()
-            if func_started and re.search("}", line.decode()):
-                par_ctr -= re.findall("}", line.decode()).__len__()
-                if par_ctr == 0:
-                    print("func ended on " + str(line_ctr))
-                    end_line = line_ctr
-                    func_started = False
-                    if(not os.path.isdir(os.path.join(folder_path, "tmp"))):
-                        subprocess.run(["mkdir",  "tmp"])
-                    with open(
-                        os.path.join(folder_path, "tmp" , os.path.splitext(os.path.basename(file_path))[0]+ '---' + func_name + ".cpp"), "wb"
-                    ) as tmp_file:
-                        # print(tmp_file.name)
-                        tmp_file.writelines(contents[start_line : end_line + 1])
+    with open(file_path, "r") as f:
+        contents = f.read()
+        contents = set(
+            re.findall(
+                r"("
+                + "(?:[a-zA-Z0-9_:&]+ )*"
+                + "(?:[\w*&]+ )?"
+                + "(?:\w*::)?"
+                + "[\w<>=+-]+"
+                + "\("
+                + "(?:\n?[ \t]*[\w,:&\* ]+ [\w,:&\*\[\]]+(?: *//.*)?[ \t]*\n?)*"
+                + " *\)"
+                + " *(?:\s*: \w+\([\w \(\)]+\) ?)?(?: *//.*)?\n?"
+                + "(?:\r?\n ?)?"
+                + "\{"
+                + "(?:(?:\n(?:\n?[\s]+.+\n)*)|(?:[\s]*\n)|(?:[^\}]+))*"
+                + "\}"
+                + ")",
+                contents,
+            )
+        )
+        # print(contents)
+        wordsmem = []
+        for fun in contents:
+            # print(fun)
+            head = fun.split("\n")[0]
+            name = ""
+            for word in head.split(" "):
+                if re.search("\(", word):
+                    name = word.split("(")[0]
+                    ctr = 1
+                    append = ""
+                    while name + append in wordsmem:
+                        append = "_" + str(ctr)
+                        ctr += 1
+                    name += append
+                    wordsmem.append(name)
+                    break
+            with open(
+                os.path.join(
+                    folder_path,
+                    "tmp",
+                    os.path.splitext(os.path.basename(file_path))[0]
+                    + "---"
+                    + name
+                    + ".cpp",
+                ),
+                "w",
+            ) as tmp_file:
+                tmp_file.writelines(fun)
+            print(name)
 
+
+if not os.path.isdir(os.path.join(folder_path, "tmp")):
+    subprocess.run(["mkdir", "tmp"])
 
 for file in os.listdir(folder_path):
     if file.endswith(".c") or file.endswith(".cpp"):
         file_path = os.path.join(folder_path, file)
         file_name = file_path.split(os.sep)[-1]
         extract_functions(file_path)
-subprocess.run(["cloc",  "--out=report.txt", "--by-file", folder_path + "/tmp/"])
+subprocess.run(["cloc", "--out=report.txt", "--by-file", folder_path + "/tmp/"])
 
 for file in os.listdir(os.path.join(folder_path, "tmp")):
     os.remove(os.path.join(folder_path, "tmp", file))
